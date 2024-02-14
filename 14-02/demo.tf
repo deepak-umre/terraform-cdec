00885ed11667abdef34f4311debd49519c213e67
deepak-umre <155575342+deepak-umre@users.noreply.github.com>
2024-02-14 10:35:34 +0530

    demo.tf added

---
+++ 14-02/demo.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "sa-east-1"
+}
+resource "aws_instance" "my-instance" {
+  ami           = data.aws_ami.my_ami.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "my_ami" {
+  most_recent = true
+
+  owners = ["self"]
+  tags = {
+    Name   = "demo_image"
+    Tested = "true"
+  }
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}

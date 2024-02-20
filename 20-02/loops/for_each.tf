e29ecac3d862d3baa246eca254054621cd1b891e
deepak-umre <155575342+deepak-umre@users.noreply.github.com>
2024-02-20 10:54:22 +0530

    for_each.tf added

---
+++ 20-02/loops/for_each.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "sa-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+    for_each = toset(var.ami_id)
+    ami = each.value
+    instance_type = "t2.micro"
+    key_name = "new-sao-paulo"
+    tags = {
+        Name = "for_each"
+    }
+}
+
+variable "ami_id" {
+   default = ["ami-05674a88169b69e3b","ami-038b785927246c729","ami-0fd8f04280ebb1e43", "ami-0fd8f04280ebb1e43"] 
+}

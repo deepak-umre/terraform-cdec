21d489b3593189598eaa9aaeb50bc597ddd450a2
deepak-umre <155575342+deepak-umre@users.noreply.github.com>
2024-02-20 10:40:16 +0530

    no change

--- 20-02/loops/count.tf
+++ 20-02/loops/count.tf
@@ -6,10 +6,10 @@ provider "aws" {
 
 resource "aws_instance" "instance" {
 	count = "4"
-	ami = "ami-05674a88169b69e3b0"
+	ami = "ami-05674a88169b69e3b"
 	instance_type ="t3.micro"
 	key_name = "new-sao-paulo"
     tags = {
         Name = "my_instance"
     }
-    }
+}
